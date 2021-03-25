a44be70f4f37418b096fe7d7d072654e3e85fd63
Adil  Hussain Mughal <57056245+Enggadil@users.noreply.github.com>
2021-03-25 04:30:33 -0700

    Add files via upload

---
+++ class 2.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x="""hello wolrd"""
+
+
+# In[ ]:
+
+
+x
+
+
+# In[ ]:
+
+
+print(type(x))
+
+
+# In[ ]:
+
+
+x=8
+
+
+# In[ ]:
+
+
+type(x)
+
+
+# In[ ]:
+
+
+print("enter your name:")
+name=input()
+print(name)
+
+
+# In[ ]:
+
+
+name=input("enter your name:")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+x=12
+y=2.8
+z="abc"
+p=23j
+
+
+# In[ ]:
+
+
+print(type(x))
+
+
+# In[ ]:
+
+
+print(type(y))
+
+
+# In[ ]:
+
+
+print(type(z))
+
+
+# In[ ]:
+
+
+print(type(p))
+
+
+# In[ ]:
+
+
+y
+
+
+# In[ ]:
+
+
+x
+
+
+# In[ ]:
+
+
+z
+
+
+# In[ ]:
+
+
+p
+
+
+# In[ ]:
+
+
+print(x)
+
+
+# In[ ]:
+
+
+print(id(x))
+
+
+# In[ ]:
+
+
+print(id(y))
+
+
+# In[ ]:
+
+
+print(id(p))
+
+
+# In[ ]:
+
+
+print(x+y)
+
+
+# In[ ]:
+
+
+print(str(x)+z)
+
+
+# In[ ]:
+
+
+x=12
+y=2.8
+z="abc"
+p=23j
+n="xyz"
+
+
+# In[ ]:
+
+
+print(z+n)
+
+
+# In[7]:
+
+
+#num1=int(input("please enter your first number:"))
+
+
+# In[8]:
+
+
+#num2=int(input("please enter your second  number:"))
+
+
+# In[9]:
+
+
+#add=num1+num2
+
+
+# In[10]:
+
+
+#print(add)
+
+
+# In[16]:
+
+
+num1=input("please enter your first number:")
+
+
+# In[17]:
+
+
+num2=input("please enter your second number:")
+
+
+# In[18]:
+
+
+print(int(num1)+int(num2))
+
+
+# In[22]:
+
+
+str1="hello pakistan "
+
+
+# In[23]:
+
+
+str2="dil dil pakistan"
+
+
+# In[24]:
+
+
+print(str1+str2)
+
+
+# In[25]:
+
+
+x=10
+y=12
+sub=x-y
+sub
+
+
+# In[26]:
+
+
+x=10
+y=12
+mul=x*y
+mul
+
+
+# In[27]:
+
+
+x=10
+y=12
+div=x/y
+div
+
+
+# In[30]:
+
+
+x=10
+y=12
+rem=y%x
+rem
+
+
+# In[36]:
+
+
+print(10*"hello pakistan\n ")
+
+
+# In[54]:
+
+
+import random
+
+print(random.randint(0,100))
+
+
+# In[56]:
+
+
+x = 5
+y = 10
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values
+temp = x # 5
+x = y# 10
+y = temp#5
+
+x
+
+
+# In[57]:
+
+
+y
+
+
+# In[58]:
+
+
+x = 5
+y = 10
+
+x, y = y, x
+print("x =", x)
+print("y =", y)
+
+
+# In[59]:
+
+
+x = x + y
+y = x - y
+x = x - y
+
+
+# In[60]:
+
+
+x
+
+
+# In[61]:
+
+
+y
+
+
+# In[63]:
+
+
+a = 6
+b = 4
+  
+maximum = max(a, b) 
+print(maximum) 
+
+
+# In[ ]:
+
+
+
+
