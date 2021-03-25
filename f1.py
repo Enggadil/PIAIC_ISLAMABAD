7d1b3463a194459c2df8b373233907b969191ba4
Adil  Hussain Mughal <57056245+Enggadil@users.noreply.github.com>
2021-03-25 04:30:13 -0700

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # heading
+
+# In[3]:
+
+
+t
+
+
+# In[8]:
+
+
+my_name="Adil Hussain"
+father_name="Ghulam Murzata"
+university="NUML"
+cnic=111111111111
+city="isb"
+message= """
+Learn Python With Adil
+Name= {}
+Father Name= {}
+University= {}
+CNIC= {}
+city={}
+""".format(my_name, father_name, university, cnic,city)
+
+print(message)
+
+
+# In[12]:
+
+
+a=1
+b=7
+add=a+b #add=8
+print(add)
+
+
+# In[ ]:
+
+
+
+
